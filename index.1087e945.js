d328b77c6e708a48ea7418c2cb542be5af2136f7
borinskij <108678236+borinskij@users.noreply.github.com>
2022-09-22 11:15:21 +0000

    Deploying to gh-pages from @ borinskij/goit-js-hw-11@56c35fb151aa67d98c9420a561ae2727bc9e084d 🚀

---
+++ index.1087e945.js
@@ -0,0 +1,2 @@
+fetch("https://jsonplaceholder.typicode.com/posts").then((function(n){return n.json()})).then((function(n){return console.log(n)})).catch((function(n){return console.log(n)}));
+//# sourceMappingURL=index.1087e945.js.map
---
+++ index.3a5ea835.js
@@ -0,0 +1,2 @@
+fetch("https://jsonplaceholder.typicode.com/posts").then((o=>o.json())).then((o=>console.log(o))).catch((o=>console.log(o)));
+//# sourceMappingURL=index.3a5ea835.js.map
